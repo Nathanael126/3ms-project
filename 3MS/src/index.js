1a2170bf5424b8371a0caf8954c9eb265c87b70f
Nathanael Setiawan <metalwhine@gmail.com>
2022-05-31 23:11:35 +0700

    Delete YEET

--- 3MS/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import LoginPage from "./components/loginPage.jsx";
-
-ReactDOM.render(<LoginPage />, document.getElementById('root'));
