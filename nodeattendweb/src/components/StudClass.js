878dda0f5cf9c782528eb9a6db04d9cdcf08fd98
JaysM32 <71362967+JaysM32@users.noreply.github.com>
2022-06-06 01:52:20 +0700

    Update StudClass.js

--- nodeattendweb/src/components/StudClass.js
+++ nodeattendweb/src/components/StudClass.js
@@ -26,20 +26,6 @@ export default function Dashboard() {
     }
   }
 
-  // PHP Input handling
-  const [inputs,setInputs] = useState({})
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]:value}));
-  }
-
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    // axios.post
-    console.log(inputs);
-  }
   return (
 
     <div className="page">
